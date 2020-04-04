3fa08e1f0d2ba108f583f57ee4e8edb080426949
Rafael <rafael.endemann@gmail.com>
2020-04-04 22:16:50 +0200

    Don't force https

--- app.js
+++ app.js
@@ -17,15 +17,6 @@ function listen(){
   console.log('Codenames Server Started at http://' + host + ':' + port);
 }
 
-// Force SSL
-app.use((req, res, next) => {
-  if (req.header('x-forwarded-proto') !== 'https') {
-    res.redirect(`https://${req.header('host')}${req.url}`)
-  } else {
-    next();
-  }
-});
-
 // Files for client
 app.use(express.static('public'))
 
